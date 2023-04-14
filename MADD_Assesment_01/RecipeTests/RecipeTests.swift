84622254cc8f8401154f12a0b29a42f8bb12d754
Chamodh <IT20189044@my.sliit.lk>
2023-04-14 14:41:54 +0530

    Unit testing file created

---
+++ MADD_Assesment_01/RecipeTests/RecipeTests.swift
@@ -0,0 +1,25 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Chamodh Abeygoonawardana on 2023-04-14.
+//
+
+import XCTest
+
+final class RecipeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCookingTimeLargerThanTen() throws {
+//        let recipe = RecipeCustom(name: "Sausages", recipeDescription: "This is description", imageName: "NSDATA", ingredients: "These are ingredients", recipeCategory: "This is category")
+    }
+
+
+}
